18d1daac1d771cf69ade99d2a91f93552d93f254
rasisrael <israel.uzcategui@outlook.com>
2024-09-17 08:41:38 -0400

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BartmartWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
