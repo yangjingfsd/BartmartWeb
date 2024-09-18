ea08586167afd2c772ffee04f22f6e5e495a6e7c
KarinaDeVargasPereira <KarinaDeVargasPereira@users.noreply.github.com>
2024-09-18 12:38:38 -0400

    AdminModel ready to be used

---
+++ Models/AdminModel.cs
@@ -0,0 +1,11 @@
+﻿namespace BartmartWeb.Models
+{
+    public class AdminModel
+    {
+        public string AdminID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+
+    }
+}
