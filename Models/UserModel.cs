0537d6691aa0e244e6f241ad03172dfd3d95d6c9
KarinaDeVargasPereira <KarinaDeVargasPereira@users.noreply.github.com>
2024-09-18 12:33:45 -0400

    UserModel ready to be used

--- Models/UserModel.cs
+++ Models/UserModel.cs
@@ -2,6 +2,18 @@
 {
     public class UserModel
     {
-
+        public string UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string PostalCode { get; set; }
+        public DateTime RegisteredDate { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string ProfilePicture { get; set; }
+        public List<string> Listings { get; set; }
+        public List<string> Conversations { get; set; }
+        public List<string> Wishlist { get; set; }
     }
 }
